b0957b355a5431e7cca272db4cf1a1dfa0bc7062
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-09-14 17:28:50 +0300

    My office Computer

---
+++ JS_advanced/sameNumbers.js
@@ -0,0 +1,10 @@
+function sameNumbers(num) {
+    let numAsString = num.toString();
+    let firstDigit = numAsString[0];
+    let isSame = true;
+    for (let char of numAsString) {
+        
+    }
+}
+
+sameNumbers(2222222);
---
+++ JS_advanced/wordsToUpperCase.js
@@ -0,0 +1,11 @@
+function wordsUppercase(text) {
+    for (let i = 0; i < text.length; i++) {
+        let charCode = text.charCodeAt(i);
+        if (charCode >= 101 && charCode <= 132 || charCode >= 97 && charCode <= 172) {
+            
+        }
+    }
+}
+
+
+wordsUppercase('Hi, how are you?')
